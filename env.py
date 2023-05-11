a915c7dc9b5d929ab1b7cd43d8e78838813d86f0
AkritW <mail2max.w@gmail.com>
2023-05-11 14:44:57 +0800

    add env schema

---
+++ env.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings, Field
+
+
+class Env(BaseSettings):
+    TEST_EMAIL: str = Field(..., min_length=1)
+    TEST_PASSWORD: str = Field(..., min_length=1)
+    TEST_DESTINATION_EMAIL: str = Field(..., min_length=1)
+
+    class Config:
+        case_sensitive = True
+
+
+env = Env(_env_file=".env")  # type: ignore
