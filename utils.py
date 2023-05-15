ab2e5dddcd77b8fdf5bf981063ecae1618b01602
AkritW <mail2max.w@gmail.com>
2023-05-15 16:35:30 +0800

    add is_type function to utils

--- utils.py
+++ utils.py
@@ -1,2 +1,17 @@
-def compose(f, g):
+from pydantic import BaseModel
+from typing import TypeVar, Callable, Type, Any, Dict
+
+T = TypeVar("T")
+F = TypeVar("F")
+
+
+def compose(f: Callable[..., F], g: Callable[..., Any]) -> Callable[..., F]:
     return lambda *a, **kw: f(g(*a, **kw))
+
+
+def is_type(data: Any, Data: Callable[..., Any]) -> bool:
+    try:
+        Data(**data)
+        return True
+    except ValueError:
+        return False
