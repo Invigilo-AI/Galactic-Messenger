69940b8651be3903ff5142121e0686ecd3862355
AkritW <mail2max.w@gmail.com>
2023-05-16 15:30:23 +0800

    update env

--- env.py
+++ env.py
@@ -5,6 +5,8 @@ class Env(BaseSettings):
     TEST_EMAIL: str = Field(..., min_length=1)
     TEST_PASSWORD: str = Field(..., min_length=1)
     TEST_DESTINATION_EMAIL: str = Field(..., min_length=1)
+    TEST_WHATSAPP_IP: str = Field(..., min_length=1)
+    TEST_WHATSAPP_GROUP_ID: str = Field(..., min_length=1)
 
     class Config:
         case_sensitive = True
