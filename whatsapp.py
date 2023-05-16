0d1737aca043e1b2471ade906d9f969cf623b023
AkritW <mail2max.w@gmail.com>
2023-05-16 15:31:10 +0800

    update and tested whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,23 +1,25 @@
-import aiohttp
-from config import Config
-from typing import cast, Literal, List, Union
+import base64
 from functools import partial
-from utils import compose, is_type
-from pydantic import BaseModel
+from typing import List, Literal, TypedDict, Union, cast
+
+import aiohttp
+
+from config import Config
+from utils import compose, is_schema
 
 
-class WhatsappMessagePayload(BaseModel):
+class WhatsappMessagePayload(TypedDict):
     groupId: str
     message: str
 
 
-class WhatsappImagePayload(BaseModel):
+class WhatsappImagePayload(TypedDict):
     groupId: str
     caption: str
     imageBase64: str
 
 
-class WhatsappVideoPayload(BaseModel):
+class WhatsappVideoPayload(TypedDict):
     groupId: str
     caption: str
     videoBase64: str
@@ -36,14 +38,42 @@ AllowedWhatsappPayload = Union[
 ]
 
 
+class WhatsappMessageInput(TypedDict):
+    groupId: str
+    message: str
+
+
+class WhatsappImageInput(TypedDict):
+    groupId: str
+    caption: str
+    imageBytes: bytes
+
+
+class WhatsappVideoInput(TypedDict):
+    groupId: str
+    caption: str
+    videoBytes: bytes
+
+
+SingleWhatsappInput = Union[
+    WhatsappMessageInput, WhatsappImageInput, WhatsappVideoInput
+]
+
+BatchWhatsappInput = List[
+    Union[WhatsappMessageInput, WhatsappImageInput, WhatsappVideoInput]
+]
+
+WhatsappInput = Union[SingleWhatsappInput, BatchWhatsappInput]
+
+
 def _get_payload_type(
     payload: AllowedSingleWhatsappPayload,
 ) -> Literal["MESSAGE", "IMAGE", "VIDEO"]:
-    if is_type(payload, WhatsappMessagePayload):
+    if is_schema(payload, WhatsappMessagePayload):
         return "MESSAGE"
-    elif is_type(payload, WhatsappImagePayload):
+    elif is_schema(payload, WhatsappImagePayload):
         return "IMAGE"
-    elif is_type(payload, WhatsappImagePayload):
+    elif is_schema(payload, WhatsappVideoPayload):
         return "VIDEO"
     else:
         raise ValueError("Invalid Input Payload")
@@ -59,7 +89,9 @@ async def _send(
     payload_type: Literal["MESSAGE", "IMAGE", "VIDEO"],
     payload: AllowedSingleWhatsappPayload,
 ) -> aiohttp.ClientResponse:
-    response = await session.post(f"{ip}/{payload_type.lower()}", json=payload)
+    response = await session.post(
+        f"{ip}/sendWhatsapp/{payload_type.lower()}", json=payload
+    )
     return response
 
 
@@ -72,14 +104,14 @@ async def _send_and_parse_to_json(
     return await _to_json(await _send(ip, session, payload_type, payload))
 
 
-async def _get_type_and_send_single_and_parse_to_json(
+async def _get_type_and_send_and_parse_to_json(
     ip: str,
     session: aiohttp.ClientSession,
     payload: AllowedSingleWhatsappPayload,
 ):
     return await partial(
         _send_and_parse_to_json, ip=ip, session=session, payload=payload
-    )(_get_payload_type(payload))
+    )(payload_type=_get_payload_type(payload))
 
 
 async def _send_single(
@@ -87,9 +119,7 @@ async def _send_single(
     session: aiohttp.ClientSession,
     payload: AllowedSingleWhatsappPayload,
 ):
-    return await _get_type_and_send_single_and_parse_to_json(
-        ip, session, payload
-    )
+    return await _get_type_and_send_and_parse_to_json(ip, session, payload)
 
 
 async def _send_multiple(
@@ -97,34 +127,33 @@ async def _send_multiple(
     session: aiohttp.ClientSession,
     payloads: AllowedBatchWhatsappPayload,
 ) -> list[str]:
-    return list(
-        [
-            await partial(
-                _send_single,
-                ip=ip,
-                session=session,
-            )(payload)
-            for payload in payloads
-        ],
-    )
+    return [
+        await partial(
+            _send_single,
+            ip=ip,
+            session=session,
+        )(payload=payload)
+        for payload in payloads
+    ]
 
 
 async def _handle_send(
     ip: str, session: aiohttp.ClientSession, payload: AllowedWhatsappPayload
 ) -> Union[str, list[str]]:
-    return (
-        await _send_multiple(
-            ip, session, cast(AllowedBatchWhatsappPayload, payload)
+    async with session:
+        return (
+            await _send_multiple(
+                ip, session, cast(AllowedBatchWhatsappPayload, payload)
+            )
+            if _is_batch(payload)
+            else await _send_single(
+                ip, session, cast(AllowedSingleWhatsappPayload, payload)
+            )
         )
-        if _is_batch(payload)
-        else await _send_single(
-            ip, session, cast(AllowedSingleWhatsappPayload, payload)
-        )
-    )
 
 
-def _is_batch(payload: AllowedWhatsappPayload):
-    return "BATCH" if type(payload) == "list" else "SINGLE"
+def _is_batch(payload: AllowedWhatsappPayload) -> bool:
+    return True if isinstance(payload, List) else False
 
 
 def _get_timeout_option(
@@ -145,12 +174,67 @@ def _get_timeout_option(
 
 def _handle_create_session(payload: AllowedWhatsappPayload):
     return aiohttp.ClientSession(
-        timeout=compose(_get_timeout_option, _is_batch)(payload)
+        timeout=compose(
+            _get_timeout_option,
+            lambda x: "BATCH" if _is_batch(x) else "SINGLE",
+        )(payload)
     )
 
 
-async def setup_whatsapp(ip: str):
-    async def send_whatsapp(payload: AllowedWhatsappPayload):
+def _bytes_to_base64(b: bytes) -> str:
+    return base64.b64encode(b).decode("utf-8")
+
+
+def _parse_single_input_to_payload(
+    input_dict: SingleWhatsappInput,
+) -> AllowedSingleWhatsappPayload:
+    if is_schema(input_dict, WhatsappMessageInput):
+        wa_mi = cast(WhatsappMessageInput, input_dict)
+        return {
+            "groupId": wa_mi["groupId"],
+            "message": wa_mi["message"],
+        }
+    elif is_schema(input_dict, WhatsappImageInput):
+        wa_ii = cast(WhatsappImageInput, input_dict)
+        return {
+            "groupId": wa_ii["groupId"],
+            "imageBase64": _bytes_to_base64(wa_ii["imageBytes"]),
+            "caption": wa_ii["caption"],
+        }
+    elif is_schema(input_dict, WhatsappVideoInput):
+        wa_ii = cast(WhatsappVideoInput, input_dict)
+        return {
+            "groupId": wa_ii["groupId"],
+            "videoBase64": _bytes_to_base64(wa_ii["videoBytes"]),
+            "caption": wa_ii["caption"],
+        }
+    else:
+        raise ValueError("Input Schema is Invalid")
+
+
+def _parse_multiple_input_to_payload(
+    input_dicts: BatchWhatsappInput,
+) -> AllowedBatchWhatsappPayload:
+    return list(map(_parse_single_input_to_payload, input_dicts))
+
+
+def _handle_parse_input_to_payload(
+    whatsapp_input: WhatsappInput,
+) -> AllowedWhatsappPayload:
+    return (
+        _parse_multiple_input_to_payload(
+            cast(BatchWhatsappInput, whatsapp_input)
+        )
+        if isinstance(whatsapp_input, List)
+        else _parse_single_input_to_payload(
+            cast(SingleWhatsappInput, whatsapp_input)
+        )
+    )
+
+
+def setup_whatsapp(ip: str):
+    async def send_whatsapp(whatsapp_input: WhatsappInput):
+        payload = _handle_parse_input_to_payload(whatsapp_input)
         return await _handle_send(ip, _handle_create_session(payload), payload)
 
     return send_whatsapp
