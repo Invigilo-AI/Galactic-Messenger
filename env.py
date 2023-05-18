3ee08745823fd3379234c19fad10ceb288b7ae14
AkritW <mail2max.w@gmail.com>
2023-05-18 14:26:08 +0800

    add end to end test for telegram

--- env.py
+++ env.py
@@ -7,6 +7,8 @@ class Env(BaseSettings):
     TEST_DESTINATION_EMAIL: str = Field(..., min_length=1)
     TEST_WHATSAPP_IP: str = Field(..., min_length=1)
     TEST_WHATSAPP_GROUP_ID: str = Field(..., min_length=1)
+    TEST_TELEGRAM_TOKEN: str = Field(..., min_length=1)
+    TEST_TELEGRAM_CHAT_ID: str = Field(..., min_length=1)
 
     class Config:
         case_sensitive = True
